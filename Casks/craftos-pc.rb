099da124a208451722747aa0ddc7a4312f28ac85
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-11-24 17:32:55 -0500

    Update craftos-pc.rb

--- Casks/craftos-pc.rb
+++ Casks/craftos-pc.rb
@@ -1,6 +1,6 @@
 cask 'craftos-pc' do
-  version '2.4'
-  sha256 '8ae223f6705842c4355dba55254df4887d92b48ae5a59abdaaad894fb43a1130'
+  version '2.4.4'
+  sha256 '40261236f13a9a7fa84ed9c26f93ffaebe06907017ead9f2f3f1e1f3a32937fc'
 
   url "https://github.com/MCJack123/craftos2/releases/download/v#{version}/CraftOS-PC.dmg"
   appcast 'https://github.com/MCJack123/craftos2/releases.atom'
