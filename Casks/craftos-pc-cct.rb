168820e003a55c8bfa56deb0f137267b5c356d38
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-12-09 17:22:22 -0500

    Update craftos-pc-cct.rb

--- Casks/craftos-pc-cct.rb
+++ Casks/craftos-pc-cct.rb
@@ -1,6 +1,6 @@
 cask 'craftos-pc-cct' do
-  version '2.4.4'
-  sha256 '693956460708227592ee666f5bd1620d79b6feaf3698b1860d2f606c49ccd45d'
+  version '2.4.5'
+  sha256 '04669a13b27c72d059f33c4e6c5f4d56ebc235289be5fa475c2abf170764f0c3'
 
   url "https://github.com/MCJack123/craftos2/releases/download/v#{version}/CraftOS-PC-CCT-Edition.dmg"
   appcast 'https://github.com/MCJack123/craftos2/releases.atom'
