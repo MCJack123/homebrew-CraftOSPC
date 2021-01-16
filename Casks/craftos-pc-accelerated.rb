eb58c0b76461ba8d97e7aae583601bfc358b4f8d
JackMacWindows <jackmacwindowslinux@gmail.com>
2021-01-16 00:37:07 -0500

    Update craftos-pc-accelerated.rb

--- Casks/craftos-pc-accelerated.rb
+++ Casks/craftos-pc-accelerated.rb
@@ -1,6 +1,6 @@
 cask 'craftos-pc-accelerated' do
-  version '2.4.5'
-  sha256 '8f0543142be3d11245091981a2f095ba4074a89fc8ef8e52c9e26193c74feed4'
+  version '2.5.1.1'
+  sha256 'fcc9e55986f141278d0be6be72a7fff28bf73c578427e4531ab2dcca9c815b2a'
 
   url "https://github.com/MCJack123/craftos2/releases/download/v#{version}-luajit/CraftOS-PC.dmg"
   appcast 'https://github.com/MCJack123/craftos2/releases.atom'
