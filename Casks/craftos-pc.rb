2eb5eaa46ecfdf8a6831b74f4684d52b4edf9054
JackMacWindows <jackmacwindowslinux@gmail.com>
2023-07-27 18:22:21 -0400

    Fixed #2

--- Casks/craftos-pc.rb
+++ Casks/craftos-pc.rb
@@ -3,7 +3,10 @@ cask "craftos-pc" do
   sha256 "838b2576a697db310eea186c50b5c55b710dca2f4cc782a8e6f16c1b97c0ed80"
 
   url "https://github.com/MCJack123/craftos2/releases/download/v#{version}/CraftOS-PC.dmg"
-  appcast "https://github.com/MCJack123/craftos2/releases.atom"
+  livecheck do
+    url "https://github.com/MCJack123/craftos2/releases/latest/download/CraftOS-PC.dmg"
+    strategy :header_match
+  end
   name "CraftOS-PC"
   homepage "https://github.com/MCJack123/craftos2/"
 
